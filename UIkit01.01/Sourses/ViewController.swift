83b78766e6d5c5cb2a89785e00d923f96ac502bd
ivan <houndby@gmail.com>
2022-12-22 17:37:59 +0300

    makin some UI elements

--- UIkit01.01/Sourses/ViewController.swift
+++ UIkit01.01/Sourses/ViewController.swift
@@ -5,18 +5,45 @@ class ViewController: UIViewController {
     
     //MARK: -> UI elements
     
+    private lazy var labelLOgin: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .heavy)
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var background: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(named: "background")
+        return imageView
+    }()
+    
+    
     //MARK: -> Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
-        // Do any additional setup after loading the view.
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
     
     //MARK: -> Setup
     
+    private func setupView() {
+        
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
     //MARK: -> Actions
-
-
 }
 
